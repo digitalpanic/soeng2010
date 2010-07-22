48e4759db2c2ea0233a2e6e59b8666aa941d580d
leicherbua <leicherbua@cda54e7b-a85e-7ef8-b3ce-0c745c2e6de1>
2010-07-22 23:44:01 +0000

    Auslastungsmitzählung eingebaut

--- src/flugBuchSys/app/BuchungAbsetzen.java
+++ src/flugBuchSys/app/BuchungAbsetzen.java
@@ -10,42 +10,83 @@ import flugBuchSys.odbc.UpdateQueries;
 public class BuchungAbsetzen {
 
 	// Eingaben der Benutzer für die Buchung holen
-	public static String  rueck = JFrameNeueBuchung.rueck;;
+	public static String rueck = JFrameNeueBuchung.rueck;;
 	public static String strVon = JFrameNeueBuchung.jTextFieldVon.getText();
 	public static String strNach = JFrameNeueBuchung.jTextFieldNach.getText();
-	public static String strHinflug = JFrameNeueBuchung.jTextFieldAbflugDatum.getText();
-	public static String strRueckflug = JFrameNeueBuchung.jTextFieldRueckflugDatum.getText();
-	public static String strServiceKlasse = (String) JFrameNeueBuchung.jComboBoxServiceKlasse.getSelectedItem();
-	public static String strVorname = JFrameNeueBuchung.jTextFieldVorname.getText();
-	public static String strNachname = JFrameNeueBuchung.jTextFieldNachname.getText();
-	public static int personenzahl = Integer.parseInt(JFrameNeueBuchung.jTextFieldPersonenZahl.getText());
+	public static String strHinflug = JFrameNeueBuchung.jTextFieldAbflugDatum
+			.getText();
+	public static String strRueckflug = JFrameNeueBuchung.jTextFieldRueckflugDatum
+			.getText();
+	public static String strServiceKlasse = (String) JFrameNeueBuchung.jComboBoxServiceKlasse
+			.getSelectedItem();
+	public static String strVorname = JFrameNeueBuchung.jTextFieldVorname
+			.getText();
+	public static String strNachname = JFrameNeueBuchung.jTextFieldNachname
+			.getText();
+	public static int personenzahl = Integer
+			.parseInt(JFrameNeueBuchung.jTextFieldPersonenZahl.getText());
 	// Eingaben der Benutzer für die Gepaeckbuchung holen
-	static int gepaeckstuecke = Integer.parseInt(JFrameGepaeckBuchen.jTextFieldGepackstuecke.getText());
+	static int gepaeckstuecke = Integer
+			.parseInt(JFrameGepaeckBuchen.jTextFieldGepackstuecke.getText());
 
 	public static String GenerateInsertString() {
-	// Insert String zusammenbauen
-	String Statement = ("INSERT INTO RESERVIERUNG (KUNDENNUMMER, FLUGID, PASSAGIERE, RUECKFLUG, KLASSE, ACTIVE, GEPAECK) VALUES ((SELECT KUNDENNUMMER FROM KUNDE WHERE VORNAME LIKE '%"
-			+ strVorname
-			+ "%' AND NACHNAME LIKE '%"
-			+ strNachname
-			+ "%'), (SELECT FLUGID FROM FLUG WHERE ROUTE = (SELECT ROUTEID FROM ROUTE WHERE VON = (SELECT FLUGHAFENID FROM FLUGHAFEN WHERE LANGTEXT LIKE '%"
-			+ strVon
-			+ "%') AND NACH = (SELECT FLUGHAFENID FROM FLUGHAFEN WHERE LANGTEXT LIKE '%"
-			+ strNach
-			+ "%'))), "
-			+ personenzahl
-			+ ", '"
-			+ rueck
-			+ "', '"
-			+ strServiceKlasse + "', 'J', "+gepaeckstuecke+")");
+		// Insert String zusammenbauen
+		String Statement = ("INSERT INTO RESERVIERUNG (KUNDENNUMMER, FLUGID, PASSAGIERE, RUECKFLUG, KLASSE, ACTIVE, GEPAECK) VALUES ((SELECT KUNDENNUMMER FROM KUNDE WHERE VORNAME LIKE '%"
+				+ strVorname
+				+ "%' AND NACHNAME LIKE '%"
+				+ strNachname
+				+ "%'), (SELECT FLUGID FROM FLUG WHERE ROUTE = (SELECT ROUTEID FROM ROUTE WHERE VON = (SELECT FLUGHAFENID FROM FLUGHAFEN WHERE LANGTEXT LIKE '%"
+				+ strVon
+				+ "%') AND NACH = (SELECT FLUGHAFENID FROM FLUGHAFEN WHERE LANGTEXT LIKE '%"
+				+ strNach
+				+ "%'))), "
+				+ personenzahl
+				+ ", '"
+				+ rueck
+				+ "', '"
+				+ strServiceKlasse + "', 'J', " + gepaeckstuecke + ")");
 
-	// Insert String zurückgeben
-	return Statement;
+		// Insert String zurückgeben
+		return Statement;
 
-}
-	public static void DatenSpeichern (){
+	}
+
+	public static String UpdateAuslasung(){
+		
+		// Umbenennen derKlassen für die Datenbank
+		String Klasse = null;
+		if (strServiceKlasse == "First") {
+			Klasse = ("AUSLFIRST");
+		} else if (strServiceKlasse == "Economy") {
+			Klasse = ("AUSLECO");
+		} else if (strServiceKlasse == "Business") {
+			Klasse = ("AUSLBUIS");
+		}		
+		
+		// String zusammensetzen
+		String statementUpdate = ("UPDATE FLUG SET "	
+				+ Klasse 
+				+ " = " 
+				+ Klasse 
+				+ " + " 
+				+ personenzahl 
+				+ " WHERE FLUGID = (SELECT FLUGID FROM FLUG WHERE ROUTE = (SELECT ROUTEID FROM ROUTE WHERE VON = (SELECT FLUGHAFENID FROM FLUGHAFEN WHERE LANGTEXT LIKE '%"
+				+ strVon
+				+ "%') AND NACH = (SELECT FLUGHAFENID FROM FLUGHAFEN WHERE LANGTEXT LIKE '%"
+				+ strNach
+				+ "%')))");
+		System.out.println(statementUpdate);
+		return statementUpdate;
+		
+	}
+
+	public static void DatenSpeichern() {
 		UpdateQueries.update(GenerateInsertString());
+		UpdateQueries.update(UpdateAuslasung());
 	}
 	
-	
+	public static void SetAusl(){
+		UpdateQueries.update(UpdateAuslasung());
+	}
+
 }
